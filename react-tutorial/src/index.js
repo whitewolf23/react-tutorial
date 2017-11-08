6d5871d2aa2573e2700f0f71e1365d802fe6bfd8
김민수 <whitewolf23@naver.com>
2017-11-08 19:55:13 +0900

    [개발]20171108
    작업환경 설정

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,7 @@
+// webpack entry point 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
