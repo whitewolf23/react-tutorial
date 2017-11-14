bd1373e3c734d3d4eaab3a8b41a935617edfcd60
김민수 <whitewolf23@naver.com>
2017-11-14 20:04:47 +0900

    리액트 1편 2편

---
+++ react-tutorial/src/components/App.js
@@ -0,0 +1,14 @@
+// app components
+import React from 'react';
+
+class App extends React.Component {
+   //컴포넌트에 렌더링 될 데이터를 가져온다 .
+    render(){
+
+        return (
+                <h1>Hello Minsu</h1>
+        );
+    }
+}
+
+export default App;
