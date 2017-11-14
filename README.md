b0b77a495c7b8783bb479f19ee0b8c7cc044f80c
김민수 <whitewolf23@naver.com>
2017-11-14 20:03:44 +0900

    no message

--- README.md
+++
@@ -1,5 +0,0 @@
-# react-tutorial
-
-#11월 8일 
-- 작업환경 설정
-- https://velopert.com/814
--- 환경설정 구축/src/index.js
+++
@@ -1,7 +0,0 @@
-// webpack entry point 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
